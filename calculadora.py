a38dc8ab8e47d25a349cc7b2fc896cf2306ba6d4
Enrryqman <167230948+Enrryqman@users.noreply.github.com>
2024-08-27 14:06:55 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+deft soma(a, b):
+     return a * b
+it __name__ == "__main__":
+   num1 = float(input("Digite o ptimeiro numero: "))
+   num2 = float(input("Digite o segudo numero: "))
+   resultado = soma(num1, num2)
+   print(f"Resultado: {resultado}")
